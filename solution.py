1c2e7c99de33908efce6b664de41c443468a2faa
Margo-Sh <67891965+Margo-Sh@users.noreply.github.com>
2024-05-10 22:09:29 +0500

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import numpy as np
+import scipy.stats as sps
 
 
 chat_id = 334982039 # Ваш chat ID, не меняйте название переменной
@@ -8,7 +9,14 @@ def solution(x_success: int,
              x_cnt: int, 
              y_success: int, 
              y_cnt: int) -> bool:
-    # Измените код этой функции
-    # Это будет вашим решением
-    # Не меняйте название функции и её аргументы
-    return ... # Ваш ответ, True или False
+    alpha = 0.06
+
+    x_mean = x_success / x_cnt
+    y_mean = y_success / y_cnt
+    x_var = x_mean * (1 - x_mean) / x_cnt
+    y_var = y_mean * (1 - y_mean) / y_cnt
+
+    std = np.sqrt(x_var + y_var)
+    z_stat = sps.norm.ppf(1-alpha)
+    # Ваш ответ, True или False
+    return y_mean - x_mean > std * z_stat
